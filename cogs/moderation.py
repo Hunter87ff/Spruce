b0fcc26b8ff66fe44c6eff8b4bd6536f1f923205
hunter87ff <huntergaming702@gmail.com>
2022-03-29 00:37:53 +0530

    updated

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -14,12 +14,12 @@ class Moderation(commands.Cog):
 	@commands.has_permissions(administrator=True)
 	async def ra_role(self, ctx, role:discord.Role):
 		for member in ctx.guild.members:
-			if member.role > ctx.author.role:
+			if member.top_role > ctx.author.top_role:
 				await ctx.send("You can't do this for some people")
-			if member.role < ctx.author.role:
+			if member.top_role < ctx.author.top_role:
 				if member.has_roles(role):
 					await member.remove_roles(role)
-			if bot.role < member.role:
+			if bot.top_role < member.top_role:
 				await ctx.send("I do not have enough permission")
 					
 
