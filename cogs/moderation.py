62c30ca1d89aaccd3f6a98188a414f06a011dc2b
hunter87ff <huntergaming702@gmail.com>
2022-03-29 00:01:45 +0530

    updated

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -18,17 +18,9 @@ class Moderation(commands.Cog):
 				await member.remove_roles(role)
 				await ctx.send("Done")
 				await member.send(f"{role.name} removed in {ctx.guild.name}")
-      	  
-      	  
 
 
 
-    @commands.command(aliases=['cr, crole'],help="**Use this command to crate roles\nExample: &crole Family**")
-    @commands.has_permissions(manage_roles=True)
-    async def create_roles(self, ctx, *names):
-    	for name in names:
-    		await ctx.guild.create_role(name=name)
-    		await ctx.send(f"**<:vf:947194381172084767> Role `{name}` has been created**")
 
 
 
