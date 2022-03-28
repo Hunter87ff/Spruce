56d9c35866c0de0835b325a17c817636c789a860
Hunter 87 <83773586+Hunter87ff@users.noreply.github.com>
2022-03-28 11:44:05 +0530

    update main.py

--- main.py
+++ main.py
@@ -478,6 +478,7 @@ async def create_channel(ctx,category,name):
 		 
 
 @bot.command()
+@commands.has_permissions(manage_members=True)
 async def nick(ctx, member:discord.Member, *, name):
   await member.edit(nick=name)
 		 
