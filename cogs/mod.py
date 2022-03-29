4e30da419e0b5792742b7bad196aa75dd891c67a
hunter87ff <huntergaming702@gmail.com>
2022-03-29 17:09:03 +0530

    updated

--- cogs/mod.py
+++ cogs/mod.py
@@ -26,7 +26,7 @@ class Moderation(commands.Cog):
 				return await ctx.send("Something went wrong")
 
 	@commands.command(aliases=['role'], pass_context=True,help="Use this command to give role to someone \nExample : &role  @family @hunter")
-	async def give_role(ctx,role: discord.Role, user: discord.Member):
+	async def give_role(ctx, role: discord.Role, user: discord.Member):
 		if ctx.author.top_role < role:
 			return await ctx.send("you don't have enough permission")
 		if ctx.author.top_role > role:
