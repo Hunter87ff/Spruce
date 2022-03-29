98f9061e78c34616c849ded067f55963b9643ec5
hunter87ff <huntergaming702@gmail.com>
2022-03-29 17:37:52 +0530

    updated

--- cogs/utils.py
+++ cogs/utils.py
@@ -1,1 +1,32 @@
-utils.py
+import discord
+from discord.ext import commands
+
+
+class Utility(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+		self.counter = 0
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def setup(bot):
+	bot.add_cog(Utility(bot))
