f2b727c144dc11a51bb81a45333259ea307c0144
hunter87ff <huntergaming702@gmail.com>
2022-04-01 22:59:24 +0530

    updated

--- main.py
+++ main.py
@@ -116,22 +116,6 @@ async def on_ready():
 ############################################################################################
 
 
-'''
-@bot.command(aliases=['bnr'])
-async def banner(ctx, user:discord.Member = None ):
-    if user == None:
-        user = ctx.author
-    req = await bot.http.request(discord.http.Route("GET", "/users/{uid}", uid=user.id))
-    banner_id = req["banner"]
-
-    if banner_id:
-        banner_url = f"https://cdn.discordapp.com/banners/{user.id}/{banner_id}.gif?size=1024"
-    await ctx.send(f"{banner_url}")
-
-
-'''
-
-
 
 
 @bot.command()
