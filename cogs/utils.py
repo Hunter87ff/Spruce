dd7ac1ad4d2cbe850ffc55a37b467ca5ad069531
hunter87ff <huntergaming702@gmail.com>
2022-04-01 22:09:23 +0530

    updated

--- cogs/utils.py
+++ cogs/utils.py
@@ -1,11 +1,19 @@
 import discord
 from discord.ext import commands
-
+cmd = commands
 
 class Utility(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 		self.counter = 0
+
+
+	@cmd.command()
+	async def ava(ctx, user:discord.Member):
+		if user == None:
+			user = ctx.author
+
+			return await ctx.send(user.avatar_url)
 
 
 
