115a02a0f66bb3700cb1bca4035d7e24ac06bdf3
hunter87ff <huntergaming702@gmail.com>
2022-04-29 21:29:51 +0530

    up

---
+++ cogs/template.py
@@ -0,0 +1,47 @@
+import discord
+from discord.ext import commands
+hg = commands
+gbr = "https://raw.githubusercontent.com/Hunter87ff/atomic-8/main/Game_roles/star_border.gif"
+
+
+
+class Templates(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+		self.counter = 0
+
+	@hg.command(aliases=["grole"])
+	@commands.has_permissions(manage_messages=True)
+	async def game_role(self, ctx, channel:discord.TextChannel=None):
+		if channel == None:
+			channel = ctx.channel
+
+		else:
+			ffemb = discord.Embed(title="FREE FIRE", description="**Garena Free Fire is a battle royal game. Played by millions of people. Developed by 111 dots studio and published by Garena. React on the emoji to access this game!**", color=discord.Color.blurple())
+			ffemb.set_thumbnail(url="https://raw.githubusercontent.com/Hunter87ff/atomic-8/main/Game_roles/freefire.png")
+			bgmiemb = discord.Embed(title="BGMI", description="**Battlegrounds Mobile India(BGMI), Made for players in India. It is an online multiplayer battle royale game developed and published by Krafton. React on the emoji to access this game**", color=discord.Color.blurple())
+			bgmiemb.set_thumbnail(url="https://raw.githubusercontent.com/Hunter87ff/atomic-8/main/Game_roles/bgmi.png")
+			codemb = discord.Embed(title="CALL OF DUTY", description="**Call Of Duty is a multiplayer online battle royal game, developed by TiMi Studio Group and published by Activision.react on the emoji to access this game**", color=discord.Color.blurple())
+			codemb.set_thumbnail(url="https://raw.githubusercontent.com/Hunter87ff/atomic-8/main/Game_roles/codm.png")
+			valoemb = discord.Embed(title="VALORANT", description="**Valorant is a multiplayer online battle royal game made for pc, developed and published by Riot Games. react on the emoji to access this game**", color=discord.Color.blurple())
+			valoemb.set_thumbnail(url="https://raw.githubusercontent.com/Hunter87ff/atomic-8/main/Game_roles/valorant.png")
+
+			return await ctx.send(embed=ffemb)
+			return await ctx.send(gbr)
+			return await ctx.send(embed=bgmiemb)
+			return await ctx.send(bgr)
+			return await ctx.send(embed=codemb)
+			return await ctx.send(gbr)
+			return await ctx.send(embed=valoemb)
+
+
+
+
+
+
+
+
+
+
+def setup(bot):
+    bot.add_cog(Templates(bot))
