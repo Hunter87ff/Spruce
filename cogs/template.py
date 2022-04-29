7b590fad2c0799070c9f8e91d337545fd0b3f65b
hunter87ff <huntergaming702@gmail.com>
2022-04-29 21:54:14 +0530

    up

--- cogs/template.py
+++ cogs/template.py
@@ -5,6 +5,18 @@ gbr = "https://raw.githubusercontent.com/Hunter87ff/atomic-8/main/Game_roles/sta
 
 
 
+# Raw
+ffemb = discord.Embed(title="FREE FIRE", description="**Garena Free Fire is a battle royal game. Played by millions of people. Developed by 111 dots studio and published by Garena. React on the emoji to access this game!**", color=discord.Color.blurple())
+ffemb.set_thumbnail(url="https://raw.githubusercontent.com/Hunter87ff/atomic-8/main/Game_roles/freefire.png")
+bgmiemb = discord.Embed(title="BGMI", description="**Battlegrounds Mobile India(BGMI), Made for players in India. It is an online multiplayer battle royale game developed and published by Krafton. React on the emoji to access this game**", color=discord.Color.blurple())
+bgmiemb.set_thumbnail(url="https://raw.githubusercontent.com/Hunter87ff/atomic-8/main/Game_roles/bgmi.png")
+codemb = discord.Embed(title="CALL OF DUTY", description="**Call Of Duty is a multiplayer online battle royal game, developed by TiMi Studio Group and published by Activision.react on the emoji to access this game**", color=discord.Color.blurple())
+codemb.set_thumbnail(url="https://raw.githubusercontent.com/Hunter87ff/atomic-8/main/Game_roles/codm.png")
+valoemb = discord.Embed(title="VALORANT", description="**Valorant is a multiplayer online battle royal game made for pc, developed and published by Riot Games. react on the emoji to access this game**", color=discord.Color.blurple())
+valoemb.set_thumbnail(url="https://raw.githubusercontent.com/Hunter87ff/atomic-8/main/Game_roles/valorant.png")
+
+
+
 class Templates(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
@@ -16,26 +28,15 @@ class Templates(commands.Cog):
 		if channel == None:
 			channel = ctx.channel
 
-		else:
-			ffemb = discord.Embed(title="FREE FIRE", description="**Garena Free Fire is a battle royal game. Played by millions of people. Developed by 111 dots studio and published by Garena. React on the emoji to access this game!**", color=discord.Color.blurple())
-			ffemb.set_thumbnail(url="https://raw.githubusercontent.com/Hunter87ff/atomic-8/main/Game_roles/freefire.png")
-			bgmiemb = discord.Embed(title="BGMI", description="**Battlegrounds Mobile India(BGMI), Made for players in India. It is an online multiplayer battle royale game developed and published by Krafton. React on the emoji to access this game**", color=discord.Color.blurple())
-			bgmiemb.set_thumbnail(url="https://raw.githubusercontent.com/Hunter87ff/atomic-8/main/Game_roles/bgmi.png")
-			codemb = discord.Embed(title="CALL OF DUTY", description="**Call Of Duty is a multiplayer online battle royal game, developed by TiMi Studio Group and published by Activision.react on the emoji to access this game**", color=discord.Color.blurple())
-			codemb.set_thumbnail(url="https://raw.githubusercontent.com/Hunter87ff/atomic-8/main/Game_roles/codm.png")
-			valoemb = discord.Embed(title="VALORANT", description="**Valorant is a multiplayer online battle royal game made for pc, developed and published by Riot Games. react on the emoji to access this game**", color=discord.Color.blurple())
-			valoemb.set_thumbnail(url="https://raw.githubusercontent.com/Hunter87ff/atomic-8/main/Game_roles/valorant.png")
-
-
-			return await channel.send(gbr)
-			return await channel.send(embed=ffemb)
-			return await channel.send(gbr)
-			return await channel.send(embed=bgmiemb)
-			return await channel.send(bgr)
-			return await channel.send(embed=codemb)
-			return await channel.send(gbr)
-			return await channel.send(embed=valoemb)
-			return await channel.send(gbr)
+			await channel.send(gbr)
+			await channel.send(embed=ffemb)
+			await channel.send(gbr)
+			await channel.send(embed=bgmiemb)
+			await channel.send(bgr)
+			await channel.send(embed=codemb)
+			await channel.send(gbr)
+			await channel.send(embed=valoemb)
+			await channel.send(gbr)
 
 
 
