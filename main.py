acf76fa2c5502c6c3db436cddfff5401be383a1d
hunter87ff <huntergaming702@gmail.com>
2022-04-29 21:25:15 +0530

    up

--- main.py
+++ main.py
@@ -195,49 +195,6 @@ async def tourney_setup(ctx,front,*,category=None):
     await ctx.guild.create_text_channel(str(front)+"queries", category=category, reason=reason)
     await ctx.send(f'**<:vf:947194381172084767>Successfully Created**',delete_after=5)
 
-
-
-
-
-############################################################################################
-#                                      GAME ROLES 
-############################################################################################
-    
-    
-    
-    
-gborder = "https://raw.githubusercontent.com/Hunter87ff/atomic-8/main/Game_roles/star_border.gif"
-
-ffemb = discord.Embed(title="FREE FIRE", description="**Garena Free Fire is a battle royal game. Played by millions of people. Developed by 111 dots studio and published by Garena. React on the emoji to access this game!**", color=discord.Color.blurple())
-ffemb.set_thumbnail(url="https://raw.githubusercontent.com/Hunter87ff/atomic-8/main/Game_roles/freefire.png")
-
-
-bgmiemb = discord.Embed(title="BGMI", description="**Battlegrounds Mobile India(BGMI), Made for players in India. It is an online multiplayer battle royale game developed and published by Krafton. React on the emoji to access this game**", color=discord.Color.blurple())
-bgmiemb.set_thumbnail(url="https://raw.githubusercontent.com/Hunter87ff/atomic-8/main/Game_roles/bgmi.png")
-
-
-codemb = discord.Embed(title="CALL OF DUTY", description="**Call Of Duty is a multiplayer online battle royal game, developed by TiMi Studio Group and published by Activision.react on the emoji to access this game**", color=discord.Color.blurple())
-codemb.set_thumbnail(url="https://raw.githubusercontent.com/Hunter87ff/atomic-8/main/Game_roles/codm.png")
-
-valoemb = discord.Embed(title="VALORANT", description="**Valorant is a multiplayer online battle royal game made for pc, developed and published by Riot Games. react on the emoji to access this game**", color=discord.Color.blurple())
-valoemb.set_thumbnail(url="https://raw.githubusercontent.com/Hunter87ff/atomic-8/main/Game_roles/valorant.png")
-
-
-
-
-
-
-@bot.command()
-@commands.has_permissions(manage_messages=True)
-async def grole(ctx):
-  await ctx.send(embed=valoemb)
-  await ctx.send(gborder)
-  await ctx.send(embed=codemb)
-  await ctx.send(gborder)
-  await ctx.send(embed=bgmiemb)
-  await ctx.send(gborder)
-  await ctx.send(embed=ffemb)
-
 		
 ############################################################################################
 #                                       INFO
