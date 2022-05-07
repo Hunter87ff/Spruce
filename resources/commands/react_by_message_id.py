ac22c5ccb160caf1329b5c01097ad8e2570e1207
hunter87ff <huntergaming702@gmail.com>
2022-05-07 22:27:24 +0530

    updated

--- resources/commands/react_by_message_id.py
+++
@@ -1,7 +0,0 @@
-#react command
-@bot.command()
-@commands.has_permissions(manage_messages=True)
-async def react(ctx,emoji,message_id):
-  channel = ctx.channel
-  msg = await channel.fetch_message(message_id)
-  await msg.add_reaction(emoji)
