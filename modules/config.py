c3984323248fed09fcb08b08887dc8d4d0807d57
Hunter 87 <83773586+Hunter87ff@users.noreply.github.com>
2022-07-15 21:13:28 +0530

    Create config.py

---
+++ modules/config.py
@@ -0,0 +1,13 @@
+import discord
+import pymongo
+import os
+from pymongo import MongoClient
+
+
+owner_id = 885193210455011369
+owner_tag = "hunter#6967"
+support_server = "https://discord.gg/vMnhpAyFZm"
+prefix = "&"
+maindb = MongoClient(os.environ["db_url"])
+seconddb = MongoClient(os.environ["mongo_url"])
+version = "1.8.7"
