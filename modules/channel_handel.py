93513fa721d6578e4641d47fbdef1bc83134a5f7
Hunter 87 <83773586+Hunter87ff@users.noreply.github.com>
2022-07-15 21:17:08 +0530

    Create channel_handel.py

---
+++ modules/channel_handel.py
@@ -0,0 +1,38 @@
+
+import discord
+from discord.ext import commands
+#from asyncio import sleep
+import pymongo
+from pymongo import MongoClient
+import re
+import os
+import config
+import datetime
+
+
+
+
+maindb = config.maindb
+db = maindb["tourneydb"]
+
+dbc = maindb["tourneydb"]["tourneydbc"]
+tourneydbc=dbc
+
+gtamountdbc = maindb["gtamountdb"]["gtamountdbc"]
+gtadbc = gtamountdbc
+
+
+
+
+
+
+async def ch_handel(channel):
+	tourch = dbc.find_one({"tid" : channel.id%1000000000000 })
+
+	if tourch != None:
+		gtad = gtadbc.find_one({"guild" : channel.guild.id%1000000000000})
+		gta = gtad["gta"]
+		gtadbc.update_one({"guild" : channel.guild.id%1000000000000}, {"$set" : {"gta" : gta - 1}})
+		print("A Tournament Deleted")
+
+
