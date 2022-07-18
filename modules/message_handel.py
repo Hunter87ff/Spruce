e9cf7080d23283f695fa04ef8ab7c33d3d156f19
hunter87ff <huntergaming702@gmail.com>
2022-07-18 23:21:49 +0530

    added start/pause system

--- modules/message_handel.py
+++ modules/message_handel.py
@@ -55,7 +55,11 @@ async def tourney(message):
     
     if td is None:
         return
-    
+
+    if td is not None:
+        if td["status"] == "paused":
+        return await message.author.send("Registration Paused Buddy")
+     
 
     if message.channel.id  == int(td["rch"]):
 
