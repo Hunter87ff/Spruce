878db1771d5e843c6af098076957a929ce7e3a9c
hunter87ff <huntergaming702@gmail.com>
2022-08-02 23:37:18 +0530

    678

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ import requests
 import pymongo
 import json
 from pymongo import MongoClient
-from modules import message_handel, channel_handel, checker, config, color, nitro
+from modules import (message_handel, channel_handel, checker, config, color, nitro)
 onm = message_handel
 ochd = channel_handel
 from discord.ui import Button, View
