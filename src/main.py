56f407c2018b2ab525be5ef79a9b977cfae01d46
hunter87ff <huntergaming702@gmail.com>
2022-08-03 20:08:29 +0530

    fixed  nitro function

--- src/main.py
+++ src/main.py
@@ -63,28 +63,31 @@ async def load_extensions():
 
 nitrodbc = maindb["nitrodb"]["nitrodbc"]
 async def nitrof(message):
-    webhook = discord.utils.get(await message.channel.webhooks(), name="Spruce")
-    if webhook == None:
-        webhook = await message.channel.create_webhook(name="Spruce")
-    wurl = webhook.url
-   
-            
-    words = message.content.split()
-    for word in words:
-        if word[0] == ":" and word[-1] == ":":
-            emjn = word.replace(":", "")
-            emoji = discord.utils.get(bot.emojis, name=emjn)
-            if emoji != None:
-                if emoji.name in message.content:
-                    msg = message.content.replace(":","").replace(f"{emoji.name}" , f"{emoji}")
-                gnitro = nitrodbc.find_one({"guild" : message.guild.id})
-                if gnitro == None:
-                    return
-                if gnitro != None and gnitro["nitro"] == "enabled":
-                    allowed_mentions = discord.AllowedMentions(everyone = False, roles=False, users=True)
-                    await message.delete()
-                    await webhook.send(avatar_url=message.author.display_avatar, content=msg, username=message.author.name, allowed_mentions= allowed_mentions)
+    gnitro = nitrodbc.find_one({"guild" : message.guild.id})
+    if gnitro == None:
+        return
 
+    if gnitro != None and gnitro["nitro"] == "enabled":
+        webhook = discord.utils.get(await message.channel.webhooks(), name="Spruce")
+        if webhook == None:
+            webhook = await message.channel.create_webhook(name="Spruce")
+       
+                
+        words = message.content.split()
+        for word in words:
+            if word[0] == ":" and word[-1] == ":":
+                emjn = word.replace(":", "")
+                emoji = discord.utils.get(bot.emojis, name=emjn)
+                if emoji != None:
+                    if emoji.name in message.content:
+                        msg = message.content.replace(":","").replace(f"{emoji.name}" , f"{emoji}")
+                        allowed_mentions = discord.AllowedMentions(everyone = False, roles=False, users=True)
+                        try:
+                            await message.delete()
+                            await webhook.send(avatar_url=message.author.display_avatar, content=msg, username=message.author.name, allowed_mentions= allowed_mentions)
+                        
+                        except Forbidden:
+                            await print("Missing Permissions/Something went wrong")
 
 
 @bot.event
