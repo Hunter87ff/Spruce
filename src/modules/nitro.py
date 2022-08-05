7e783edf2a8674781e06855aa405277ff2dc6e18
hunter87ff <huntergaming702@gmail.com>
2022-08-05 19:14:09 +0530

    nitro module updated

--- src/modules/nitro.py
+++ src/modules/nitro.py
@@ -2,17 +2,14 @@
 import os 
 import discord
 from discord.ext import commands
-#from asyncio import sleep
-#import datetime
-#from datetime import datetime, timedelta
 import pymongo
 from pymongo import MongoClient
-#from discord.ui import Button, View
 
 
 
-dburl = os.environ["mongo_url"]
-maindb = MongoClient(dburl)
+
+
+maindb = MongoClient(os.environ["mongo_url"])
 nitrodbc = maindb["nitrodb"]["nitrodbc"]
 
 
@@ -23,36 +20,41 @@ nitrodbc = maindb["nitrodb"]["nitrodbc"]
 
 
 async def nitrof(message):
-    ctx = message
+    if message.author.bot:
+        return
     try:
-        webhook = discord.utils.get(await message.channel.webhooks(), name="Spruce")
+        gnitro = nitrodbc.find_one({"guild" : message.guild.id})
     except:
-        await message.reply("Missing Permissions - `manage_messages` , `manage_webhooks`")
+        return
 
-    if webhook == None:
+    if gnitro != None and gnitro["nitro"] == "enabled":
         try:
-            webhook = await message.channel.create_webhook(name="Spruce")
+            webhook = discord.utils.get(await message.channel.webhooks(), name="Spruce")
+
         except:
-            await message.reply("Missing Permissions - `manage_messages` , `manage_webhooks`")
+            message.reply("Missing Permissions - `manage_messages` , `manage_webhooks`")
 
+        if webhook == None:
+            try:
+                webhook = await message.channel.create_webhook(name="Spruce")
 
-   
-            
-    words = message.content.split()
-    for word in words:
-        if word[0] == ":" and word[-1] == ":":
-            emjn = word.replace(":", "")
-            emoji = discord.utils.get(bot.emojis, name=emjn)
-            if emoji != None:
-                if emoji.name in message.content:
-                    msg = message.content.replace(":","").replace(f"{emoji.name}" , f"{emoji}")
-                gnitro = nitrodbc.find_one({"guild" : message.guild.id})
-                if gnitro == None:
-                    return
-                if gnitro != None and gnitro["nitro"] == "enabled":
-                    allowed_mentions = discord.AllowedMentions(everyone = False, roles=False, users=True)
-                    try:
+            except:
+                await message.reply("Missing Permissions - `manage_messages` , `manage_webhooks`")
+       
+                
+        words = message.content.split()
+        for word in words:
+            if word[0] == ":" and word[-1] == ":":
+                emjn = word.replace(":", "")
+                emoji = discord.utils.get(bot.emojis, name=emjn)
+                if emoji != None:
+                    if emoji.name in message.content:
+                        msg = message.content.replace(":","").replace(f"{emoji.name}" , f"{emoji}")
+                        allowed_mentions = discord.AllowedMentions(everyone = False, roles=False, users=True)
+                        nick = message.author.nick
+                        if message.author.nick == None:
+                            nick = message.author.name
+                        await webhook.send(avatar_url=message.author.display_avatar, content=msg, username=nick, allowed_mentions= allowed_mentions)
                         await message.delete()
-                        await webhook.send(avatar_url=message.author.display_avatar, content=msg, username=message.author.name, allowed_mentions= allowed_mentions)
-                    except:
-                        await message.reply("Missing Permissions - `manage_messages` , `manage_webhooks`")
+                        
+
