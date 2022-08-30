1a840c8ceb3c8ece69baed9d710d6e65573154c3
hunter87ff <huntergaming702@gmail.com>
2022-08-31 01:12:58 +0530

    updated music module

--- cogs/music.py
+++ cogs/music.py
@@ -13,21 +13,28 @@ class Music(commands.Cog):
 
 
 
+
 	@cmd.command(aliases=["p", "P"])
 	async def play(self, ctx, *, Song:wavelink.YouTubeTrack):
-	    if not ctx.voice_client:
-	        try:
-	            vc: wavelink.Player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
-	        except:
-	            return await ctx.send("Please Join a vc")
+		if not ctx.author.voice.channel != None and ctx.voice_client.channel != ctx.author.voice.channel:
+			try:
+				vc: wavelink.Player = await ctx.voice_client.move_to(ctx.author.voice.channel)   #(cls=wavelink.Player)
+			except:
+				return await ctx.send("Please Join a vc")
+		if not ctx.voice_client:
+			try:
+				vc: wavelink.Player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
+			except:
+				return await ctx.send("Please Join a vc")
 
-	    if ctx.author.voice == None:
-	        return await ctx.send("Please Join a vc")
+		if ctx.author.voice == None:
+			return await ctx.send("Please Join a vc")
 
-	    else:
-	        vc: wavelink.Player = ctx.voice_client
+		else:
+			vc: wavelink.Player = ctx.voice_client
 
-	    await vc.play(Song)
+
+		await vc.play(Song)
 	    #tracks = await wavelink.get_tracks(f'ytsearch:{Song}')
 	    #await ctx.channel.send(embed=discord.Embed(description=f'Now playing {tracks[0]} .', color=0xfd2121))
 
@@ -35,11 +42,11 @@ class Music(commands.Cog):
 
 	@cmd.command()
 	async def pause(self, ctx):
-	    if ctx.voice_client != None:
+	    if ctx.guild.voice_client.is_playing():
 	        try:
-	            await ctx.voice_client.pause()
-	            await ctx.message.delete()
+	            await ctx.guild.voice_client.pause()
 	            await ctx.send("Paused", delete_after=10)
+
 	        except:
 	            return await ctx.send("No Music Playing..")
 
@@ -58,20 +65,38 @@ class Music(commands.Cog):
 
 	@cmd.command()
 	async def resume(self, ctx):
-	    if ctx.voice_client != None:
+	    if ctx.guild.voice_client.is_playing():
 	        try:
-	            await ctx.voice_client.resume()
-	            await ctx.message.delete()
-	            await ctx.send("Resumed", delete_after=10)
+	            await ctx.guild.voice_client.pause()
+	            await ctx.send("Paused", delete_after=10)
+
 	        except:
-	            return await ctx.send("No Music Paused..")
+	            return await ctx.send("No Music Playing..")
 
 
 
+	@cmd.command()
+	async def join(self, ctx):
+		if not ctx.voice_client:
+			try:
+				await ctx.author.voice.channel.connect()
+
+			except:
+				return await ctx.send("Please Join a vc")
+
+
+		if ctx.voice_client != None:
+			try:
+				await ctx.voice_client.move_to(ctx.author.voice.channel)
+
+			except:
+				return await ctx.send("Please Join a vc")
 
 
 
-
+	@cmd.command(aliases=["vol"])
+	async def volume(self, ctx):
+		await ctx.send("Under Maintanance, will come back in few days", delete_after=5)
 
 
 
