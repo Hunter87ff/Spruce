acc6bc7584686340efd72dfed4d15ab7a916c105
Hunter 87 <83773586+Hunter87ff@users.noreply.github.com>
2022-09-16 16:20:13 +0530

    update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -280,7 +280,7 @@ class Music(commands.Cog):
 
 
 
-	@cmd.command(aliases=["disconnect"])
+	@cmd.command()
 	async def leave(self, ctx):
 		if ctx.author.voice == None:
 			return await ctx.reply("Please Join VC")
