6b1f7be05ac70eb50b26f73fdb5d1ebd965404b2
Hunter 87 <83773586+Hunter87ff@users.noreply.github.com>
2022-10-05 21:56:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from discord.ext import commands
 from asyncio import sleep
 import datetime
 from datetime import datetime, timedelta
-from data import *
 import requests
 import pymongo
 import json
