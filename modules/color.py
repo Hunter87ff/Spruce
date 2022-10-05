a051ee0defafd6f4f1818cde95a68365b9931507
hunter87ff <huntergaming702@gmail.com>
2022-10-05 21:15:31 +0530

    update config.py , color.py, tourney.py

--- modules/color.py
+++ modules/color.py
@@ -1,11 +1,10 @@
-
 blurple = 0x7289da
 greyple = 0x99aab5
 d_grey = 0x546e7a
 d_theme = 0x36393F
 l_grey = 0x979c9f
 d_red = 0x992d22
-red = 0xe74c3c
+red = 0xff0000
 d_orange = 0xa84300
 orange= 0xe67e22
 d_gold = 0xc27c0e
@@ -13,9 +12,10 @@ gold = 0xf1c40f
 magenta = 0xe91e63
 purple = 0x9b59b6
 d_blue = 0x206694 
-blue = 0x3498db
-green = 0x2ecc71
+blue = 0x0000ff
+green = 0x00ff00
 d_green = 0x1f8b4c
+pink=0xff0066
 teal = 0x1abc9c
 d_teal = 0x11806a
 yellow = 0xffff00
--- modules/config.py
+++ modules/config.py
@@ -9,4 +9,60 @@ owner_tag = "hunter#6967"
 support_server = "https://discord.gg/vMnhpAyFZm"
 prefix = "&"
 maindb = MongoClient(os.environ["mongo_url"])
-version = "1.8.7"
+version = "1.9.3"
+
+
+
+
+
+#emojis
+vf = "<:vf:959508659153535016>"
+cross = "<:cross1:975460389565399141>"
+owner = "<:owner:968371297744744448>"
+tick = "<:tick:975460256748568656>"
+partner = "<:partner:968372588533383178>"
+ping = "<:g_latency:968371843335610408>"
+music_disk = "<a:music_disk:1020370054665207888>"
+cup = "<a:cup:999246631604080711>"
+role = "<:role:1022568952573984828>"
+pubg = "<:pubg:1008283449288835073>"
+bgmi = "<:bgmi:1008283336143282216>"
+dev = "<:dev:1020696239689433139>"
+ff = "<:ff:1008282958349746176>"
+like = "<:like:1014922246072053840>" 
+mod = "<:mod:999353993035780258>"
+servers = "<:servers:1018845797556703262>"
+cod = "<:cod:1008283105452380250>"
+newstate = "<:newstate:1014922927776477205>"
+setting = "<:setting:968374105961300008>"
+valo = "<:valorant:1008283511247081542>"
+reddot = "<:reddot:973870004606992444>"
+user = "<:user:1006581167048368189>"
+defean = "<:defean:969894009331933205>"
+loading = "<a:loading:969894982024568856>"
+default_tick = "✅"
+default_cross = "❌"
+
+
+#colors
+blurple = 0x7289da
+greyple = 0x99aab5
+d_grey = 0x546e7a
+d_theme = 0x36393F
+l_grey = 0x979c9f
+d_red = 0x992d22
+red = 0xff0000
+d_orange = 0xa84300
+orange= 0xe67e22
+d_gold = 0xc27c0e
+gold = 0xf1c40f
+magenta = 0xe91e63
+purple = 0x9b59b6
+d_blue = 0x206694 
+blue = 0x0000ff
+green = 0x00ff00
+d_green = 0x1f8b4c
+pink=0xff0066
+teal = 0x1abc9c
+d_teal = 0x11806a
+yellow = 0xffff00
