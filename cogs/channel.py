1ba6d9df0617a3bed32eaa1abc3d0b6d8644729c
hunter87ff <huntergaming702@gmail.com>
2022-10-24 14:04:01 +0530

    updated delete category command ! improved security

--- cogs/channel.py
+++ cogs/channel.py
@@ -2,6 +2,20 @@ import discord
 from discord.ext import commands
 from asyncio import sleep
 cmd = commands
+from discord.ui import Button, View
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class Channel(commands.Cog):
 	def __init__(self, bot):
@@ -37,14 +51,29 @@ class Channel(commands.Cog):
 	@commands.has_permissions(administrator=True)
 	@commands.bot_has_permissions(manage_channels=True, send_messages=True, manage_messages=True)
 	async def delete_category(self, ctx, category: discord.CategoryChannel):
-		snd = await ctx.send("<a:loading:969894982024568856>**Processing...**")
-		for channel in category.channels:
-			await channel.delete(reason=f'Deleted by {ctx.author.name}')
+		if ctx.author.bot:
+			return
+		bt11 = Button(label="Confirm", style=discord.ButtonStyle.danger, custom_id="dcd_btn")
+		bt12 = Button(label="Cancel", style=discord.ButtonStyle.green, custom_id="dcc_btn")
+		view = View()
+		for i in [bt11, bt12]:
+			view.add_item(i)
+		del_t_con = await ctx.reply("**Are You Sure To Delete The Category?**", view=view)
 
-			if len(category.channels) == 0:
-				await category.delete()
-				await snd.edit(content=f'**<:vf:947194381172084767>Successfully Deleted**')
+		async def dc_confirmed(interaction):
+		    if not interaction.user.bot:
+		        await interaction.message.edit(content="<a:loading:969894982024568856>**Processing...**", view=None)
+		        for channel in category.channels:
+		            await channel.delete(reason=f'Deleted by {ctx.author.name}')
 
+		            if len(category.channels) == 1:
+		                await category.delete()
+		                return await interaction.message.edit(content=f'**<:vf:947194381172084767>Successfully Deleted**')
+		async def del_msg(interaction):
+			await interaction.message.delete()
+
+		bt11.callback = dc_confirmed
+		bt12.callback = del_msg
 
 
 
