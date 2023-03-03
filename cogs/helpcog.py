fdef7fbbba7a4cdebdcbfc6047e58efd81d51a3d
Hunter 87 <83773586+Hunter87ff@users.noreply.github.com>
2023-03-03 22:49:29 +0530

    update helpcog.py

--- cogs/helpcog.py
+++ cogs/helpcog.py
@@ -18,7 +18,7 @@ bot = commands.Bot(command_prefix=commands.when_mentioned_or("&"), intents=inten
 invbtn = Button(label="Invite", url="https://discord.com/api/oauth2/authorize?client_id=931202912888164474&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.gg%2FvMnhpAyFZm&response_type=code&scope=bot%20identify")
 votebtn = Button(label="Vote", url="https://discord.ly/spruce/upvote")
 support_serverbtn = Button(label="Support Server", url="https://discord.gg/vMnhpAyFZm")
-hel_p = "• Prefix - `&`\n• Total Commands - `78` | Usable - `73`\n• Type `&help <command | category>` for more info\n\n"
+hel_p = "• Prefix - `&`\n• Total Commands - `79` | Usable - `74`\n• Type `&help <command | category>` for more info\n\n"
 helpemb  = discord.Embed(title=f"Spruce Help Menu", description=f"{hel_p}**__Categories__\n\n<a:music:1017796831272505375> Music\n\n<:mod:999353993035780258> Moderation\n\n<:setting:968374105961300008> Utility\n\n<a:cup:999246631604080711> Esports\n\n<:role:1022568952573984828> Role**", color=0xf0ff0f)
 musicemb = discord.Embed(description=f"{hel_p}__**Musics**__\n`play`, `pause`, `resume`, `queue`, `skip`, `stop`, `join`, `leave`", color=0xf0ff0f)
 modemb   = discord.Embed(description=f"{hel_p}__**Moderation**__\n`clear`, `clear_perms`, `channel_del`, `channel_make`, `create_channel`, `delete_category`, `mute`, `unmute`, `kick`, `ban`, `hide`, `unhide`, `lock`, `unlock`, `hide_category`, `unhide_category`, `lock_category`, `unlock_category`, `setup`", color=0xf0ff0f)
