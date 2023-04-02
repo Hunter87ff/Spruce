1e4a441c9c47930d77a7f694515293ccedc1d8e5
hunter87ff <huntergaming702@gmail.com>
2023-04-03 00:50:43 +0530

    added word limit to tts

--- src/cogs/utils.py
+++ src/cogs/utils.py
@@ -135,6 +135,8 @@ class Utility(commands.Cog):
 
 	@cmd.command(aliases=['text2speech'])
 	async def tts(self, ctx, *, message):
+		if len(message.content.split()) > 100:
+			return await ctx.reply("**Up to 100 words allowed**", delete_after=30)
 		output = gTTS(text=message, lang="en", tld="co.in")
 		output.save(f"tts.mp3")
 		#fl = open("tts.mp3", r).read()
