135d849578182cbe99d8a0e353b9c8769930ee18
hunter87ff <huntergaming702@gmail.com>
2023-04-06 13:09:56 +0530

    updated ui command added banner

--- src/cogs/utils.py
+++ src/cogs/utils.py
@@ -277,6 +277,7 @@ class Utility(commands.Cog):
 	        member = ctx.author
 	    else:
 	        member = member
+	    user = self.bot.fetch_user(member.id)
 
 	    desc = f'**User Name**: {member}\n**User ID:** {member.id}\n**Nick Name:** {member.display_name}\n**Color :** {member.color.value}\n**Status:** {member.status}\n**Bot?:** {member.bot}\n**Top role:** {member.top_role.mention}\n**Created at:** {member.created_at.strftime("%a, %#d %B %Y")}\n**Joined at:** {member.joined_at.strftime("%a, %#d %B %Y")}'
 
@@ -288,8 +289,8 @@ class Utility(commands.Cog):
 	    if len(member.roles) > 8:
 	        embed.add_field(name=f"Roles ({len(roles)})", value="Too Much Roles To Show Here")  
 
-	    if member.banner:
-	        embed.set_image(url=str(member.banner))
+	    if user.banner:
+	        embed.set_image(url=str(user.banner))
 	    embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar)
 	    await ctx.send(embed=embed)
 
