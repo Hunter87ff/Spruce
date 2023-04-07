9d5eed5effa56c4e3f3823d41360b8fdf3a1af56
hunter 87 <83773586+Hunter87ff@users.noreply.github.com>
2023-04-07 08:30:22 +0530

    update channel.py

--- src/cogs/channel.py
+++ src/cogs/channel.py
@@ -59,11 +59,11 @@ class Channel(commands.Cog):
 
 
 
-	@cmd.hybrid_command(with_app_command = True)
+	@cmd.command(aliases=['dc'])
 	@commands.has_permissions(administrator=True)
 	@commands.bot_has_permissions(manage_channels=True)
 	async def delete_category(self, ctx, category: discord.CategoryChannel):
-		await ctx.defer(ephemeral=True)
+		#await ctx.defer(ephemeral=True)
 		if ctx.author.bot:
 			return
 		bt11 = Button(label="Confirm", style=discord.ButtonStyle.danger, custom_id="dcd_btn")
@@ -75,14 +75,14 @@ class Channel(commands.Cog):
 
 		async def dc_confirmed(interaction):
 		    if not interaction.user.bot:
-		        await interaction.response.send_message("<a:loading:969894982024568856>**Processing...**", ephemeral=True)
+		        await interaction.message.edit(content="<a:loading:969894982024568856>**Processing...**", view=None)
 		        #await interaction.message.delete()
 		        for channel in category.channels:
 		            await channel.delete(reason=f'Deleted by {ctx.author.name}')
 
 		            if len(category.channels) == 0:
 		                await category.delete()
-		                return await interaction.edit_original_response(content=f'**<:vf:947194381172084767>Successfully Deleted**')
+		                return await interaction.message.edit(content=f'**<:vf:947194381172084767>Successfully Deleted**')
 		async def del_msg(interaction):
 			await interaction.message.delete()
 
