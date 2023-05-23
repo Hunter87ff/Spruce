f395c682660be8d48e79667dde3fed5d5e1e6278
hunter 87 <83773586+Hunter87ff@users.noreply.github.com>
2023-05-23 07:51:21 +0530

    update src/modules/config.py

--- src/modules/config.py
+++ src/modules/config.py
@@ -100,9 +100,9 @@ async def voted(ctx, bot):
 				if f"<@{ctx.author.id}>" in i.content:
 					return "yes"
 				else:
-					return "yes"
+					return None
 		else:
-			return "yes"
+			return None
 
 
 async def vtm(ctx):
