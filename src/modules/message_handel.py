ca95a3944ed3c767811840963fa0afde80557da6
hunter 87 <83773586+Hunter87ff@users.noreply.github.com>
2023-06-17 17:22:51 +0530

    update src/modules/message_handel.py

--- src/modules/message_handel.py
+++ src/modules/message_handel.py
@@ -56,7 +56,7 @@ async def ask(message, bot):
 	if message.author.bot:
 		return
 	if message.author.id != config.owner_id:
-	return
+		return
 		if i in message.content.split():
 			return await ctx.reply("message contains blocked word. so i can't reply to this message! sorry buddy.")
 	response = sdbc.find()
