c473bb782416c626021e9525b8730bd087cb3673
hunter87ff <huntergaming702@gmail.com>
2023-08-09 10:29:30 +0530

    update

--- src/main.py
+++ src/main.py
@@ -5,68 +5,7 @@ from wavelink.ext import spotify
 from modules import (message_handel, channel_handel, config)
 #from discord.ext.commands.converter import (MemberConverter, RoleConverter, TextChannelConverter)
 from modules.bot import bot
-onm = message_handel
-ochd = channel_handel
-intents = discord.Intents.default()
-intents.message_content = True
-intents.reactions = True
-intents.members = True
-intents.voice_states = True
-intents.guilds = True
 
-#Configuring db
-pref = config.prefix
-maindb = config.maindb
-
-#bot = commands.AutoShardedBot(shard_count=10, command_prefix= commands.when_mentioned_or(pref), intents=intents) #AutoSharded
-bot.remove_command("help")
-
-async def load_extensions():
-	for filename in os.listdir(config.cogs_path):
-		if filename.endswith(".py"):
-			await bot.load_extension(f"cogs.{filename[:-3]}")
-
-@bot.event
-async def on_ready():
-	await load_extensions()
-	try:
-		await node_connect()
-		await bot.tree.sync()
-		stmsg = f'{bot.user} is ready with {len(bot.commands)} commands'
-		print(stmsg)
-		requests.post(url=config.stwbh, json={"content":"<@885193210455011369>","embeds":[{"title":"Status","description":stmsg,"color":0xff00}]})
-		while True:
-			for st in config.status:
-				await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=st))
-				await sleep(120)
-	except Exception as ex:
-		print(ex)
-
-async def node_connect():
-	try:
-		await bot.wait_until_ready()
-		await wavelink.NodePool.create_node(bot=bot,host=config.m_host,port=443,password=config.m_host_psw,https=True,spotify_client=spotify.SpotifyClient(client_id=config.spot_id,client_secret=config.spot_secret))
-	except Exception as e:
-		print(e)
-
-
-#await nitrof(message)
-#await onm.auto_grp(message, bot)
-@bot.event
-async def on_message(message):
-	if config.notuser(message):
-		return
-	await bot.process_commands(message)
-	await onm.tourney(message)
-	await onm.ask(message, bot=bot)
-	
-@bot.event
-async def on_guild_channel_delete(channel):
-    await ochd.ch_handel(channel, bot)
-	
-@bot.event
-async def on_command_error(ctx, error, bot=bot):
-    await onm.error_handle(ctx, error, bot=bot)
 
 ##########################################################################################
 #                                       COMMANDS
@@ -91,7 +30,6 @@ async def botinfo(ctx):
     emb.add_field(name=f"{config.setting} __Command Prefix__", value=f"command: {pref}help, prefix: {pref}  ", inline=False)
     emb.set_footer(text="Made with ❤️ | By hunter#6967")
     return await ctx.send(embed=emb)
-
 
 app.keep_alive()
 bot.run(config.token)
--- src/modules/bot.py
+++ src/modules/bot.py
@@ -1,22 +1,64 @@
 import discord
 from discord.ext import commands
 from discord import AllowedMentions, Intents
+from modules import (message_handel as onm, channel_handel as ochd, config)
+
+
 intents = Intents.default()
 intents.message_content = True
 intents.reactions = True
 intents.members = True
 intents.voice_states = True
 intents.guilds = True
-from modules import config
-
 
 class Spruce(commands.AutoShardedBot):
-   def __init__(self) -> None:
+  def __init__(self) -> None:
      super().__init__(
       shard_count=10, 
       command_prefix= commands.when_mentioned_or(config.prefix),
       intents=intents,
       allowed_mentions=AllowedMentions(everyone=False, roles=False, replied_user=True, users=True),
       activity=discord.Activity(type=discord.ActivityType.listening, name="&help"))
+
+  async def load_extensions():
+    for filename in os.listdir(config.cogs_path):
+      if filename.endswith(".py"):
+        await bot.load_extension(f"cogs.{filename[:-3]}")
+
+  async def on_ready():
+    await load_extensions()
+    try:
+      await self.node_connect()
+      await self.tree.sync()
+      stmsg = f'{self.user} is ready with {len(self.commands)} commands'
+      print(stmsg)
+      requests.post(url=config.stwbh, json={"content":"<@885193210455011369>","embeds":[{"title":"Status","description":stmsg,"color":0xff00}]})
+      while True:
+        for st in config.status:
+          await self.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=st))
+          await sleep(120)
+    except Exception as ex:
+      print(ex)
+
+  async def node_connect(self):
+    try:
+      await self.wait_until_ready()
+      await wavelink.NodePool.create_node(bot=self,host=config.m_host,port=443,password=config.m_host_psw,https=True,spotify_client=spotify.SpotifyClient(client_id=config.spot_id,client_secret=config.spot_secret))
+    except Exception as e:
+      print(e)
+
+  async def on_message(self, message):
+    if config.notuser(message):
+      return
+    await self.process_commands(message)
+    await onm.tourney(message)
+    await onm.ask(message, bot=self)
+
+  async def on_command_error(ctx, error, bot=bot):
+    await onm.error_handle(ctx, error, bot=bot)
+
+  async def on_guild_channel_delete(channel):
+    await ochd.ch_handel(channel, bot)
+    
 
 bot = Spruce()
