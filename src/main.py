088890c05fdaafbfb86e2a7fe3f1b78c50819899
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2023-10-31 21:59:15 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,8 @@
-try:
-  import app, os
-  from modules import config
-  from modules.bot import bot
-except:
-    os.system("pip install -r requirements.txt")
+
+import app, os
+from modules import config
+from modules.bot import bot
+
 
 app.keep_alive()
 bot.remove_command("help")
