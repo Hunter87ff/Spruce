1e9a5b9aef047cf7f24750713659cdb5fbc9a5d4
hunter87ff <huntergaming702@gmail.com>
2024-01-07 16:52:08 +0530

    update

---
+++ src/cogs/moderation.py
@@ -0,0 +1,433 @@
+import discord
+from asyncio import sleep
+from discord.ext import commands
+cmd = commands
+import datetime
+import humanfriendly
+from modules import config
+import typing
+
+class Moderation(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+		self.counter = 0
+
+	#start commands
+	
+	@cmd.hybrid_command(with_app_command = True)
+	#@commands.cooldown(2, 30, commands.BucketType.user)
+	@commands.has_permissions(manage_roles=True)
+	@commands.bot_has_permissions(manage_roles=True, send_messages=True)
+	@commands.guild_only()
+	async def lock(self, ctx, role: discord.Role=None):
+		if ctx.author.bot:
+			return
+		if not await config.voted(ctx, bot=self.bot):
+			return await config.vtm(ctx)
+		await ctx.defer()
+		#bt = ctx.guild.get_member(self.bot.user.id)
+		if ctx.author.bot:
+			return
+
+		if role == None:
+			role = ctx.guild.default_role
+		overwrite = ctx.channel.overwrites_for(role)
+		overwrite.update(send_messages=False)
+		await ctx.send(f'**<:vf:947194381172084767> Channel has been locked for `{role.name}`**', delete_after=5)
+		await ctx.channel.set_permissions(role, overwrite=overwrite)
+		 
+
+
+	@cmd.hybrid_command(with_app_command = True)
+	#@commands.cooldown(2, 30, commands.BucketType.user)
+	@commands.has_permissions(manage_roles=True)
+	@commands.guild_only()
+	@commands.bot_has_permissions(manage_roles=True)
+	async def unlock(self, ctx, role: discord.Role=None, channel:discord.TextChannel=None):
+		await ctx.defer()
+		if ctx.author.bot:
+			return
+		if not await config.voted(ctx, bot=self.bot):
+			return await config.vtm(ctx)
+		if not channel:
+			channel = ctx.channel
+		if role == None:
+			role = ctx.guild.default_role
+		overwrite = channel.overwrites_for(role)
+		overwrite.update(send_messages=True)
+		await channel.set_permissions(role, overwrite=overwrite)
+		await ctx.send(f'**<:vf:947194381172084767> {channel.mention} has been unlocked from `{role.name}`**', delete_after=5)
+
+
+
+
+	@cmd.hybrid_command(with_app_command = True)
+	@commands.has_permissions(manage_roles=True)
+	@commands.guild_only()
+	#@commands.cooldown(2, 30, commands.BucketType.user)
+	@commands.bot_has_permissions(manage_roles=True)
+	async def hide(self, ctx, role: typing.Union[discord.Role, discord.Member]=None, channel:typing.Union[discord.TextChannel, discord.VoiceChannel]=None):
+		await ctx.defer()
+		if ctx.author.bot:
+			return
+		if not await config.voted(ctx, bot=self.bot):
+			return await config.vtm(ctx)
+		if role == None:
+			role = ctx.guild.default_role
+		if channel is None:
+			channel = ctx.channel
+		overwrite = ctx.channel.overwrites_for(role)
+		overwrite.update(view_channel=False)
+		await channel.set_permissions(role, overwrite=overwrite)
+		return await ctx.send(embed=discord.Embed(description=f'{config.tick} | This channel is now hidden to {role.mention}'), delete_after=30)
+		
+		
+
+
+
+
+	@cmd.hybrid_command(with_app_command = True)
+	@commands.has_permissions(manage_roles=True)
+	@commands.cooldown(1, 60, commands.BucketType.user)
+	@commands.bot_has_permissions(manage_roles=True)
+	@commands.guild_only()
+	async def clear_perms(self, ctx, role: discord.Role=None):
+		await ctx.defer()
+		if ctx.author.bot:
+			return
+		bt = ctx.guild.get_member(self.bot.user.id)
+		if not await config.voted(ctx, bot=self.bot):
+			return await config.vtm(ctx)
+		if ctx.author.bot:
+			return
+
+		if role == None:
+			ms = await ctx.send(f"**{config.loading} Processing...**")
+			for role in ctx.guild.roles:
+				if role.position < bt.top_role.position:
+					await role.edit(permissions=discord.Permissions(permissions=0))
+					await sleep(2)
+			emb = discord.Embed(descriptio=f'{config.default_tick} | All permissions removed from all role below {bt.top_role.mention}')
+			return await ms.edit(content=None, embed=emb)
+
+
+
+		if role != None:
+			if role.position < bt.top_role.position:
+				await role.edit(permissions=discord.Permissions(permissions=0))
+				emb = discord.Embed(description=f'{config.default_tick} | All Permissions Removed from {role.mention}')
+				return await ms.edit(content=None, embed=emb)
+
+
+
+	@cmd.hybrid_command(with_app_command = True)
+	@commands.has_permissions(manage_roles=True)
+	@commands.guild_only()
+	#@commands.cooldown(2, 30, commands.BucketType.user)
+	@commands.bot_has_permissions(manage_roles=True)
+	async def unhide(self, ctx, role: typing.Union[discord.Role, discord.Member]=None, channel:typing.Union[discord.TextChannel, discord.VoiceChannel]=None):
+		if not await config.voted(ctx, bot=self.bot):
+			return await config.vtm(ctx)
+		await ctx.defer()
+		if ctx.author.bot:
+			return
+
+		if role == None:
+			role = ctx.guild.default_role
+		if channel is None:
+			channel = ctx.channel
+		overwrite = ctx.channel.overwrites_for(role)
+		overwrite.update(view_channel=True)
+		await channel.set_permissions(role, overwrite=overwrite)
+		return await ctx.send(embed=discord.Embed(description=f'{config.tick} | This channel is now visible to {role.mention}'), delete_after=30)
+		
+		
+
+
+	@cmd.hybrid_command(with_app_command = True, aliases=["lc"])
+	#@commands.cooldown(2, 30, commands.BucketType.user)
+	@commands.has_permissions(manage_roles=True)
+	@commands.bot_has_permissions(manage_roles=True)
+	@commands.guild_only()
+	async def lock_category(self, ctx,category: discord.CategoryChannel, role:discord.Role=None):
+		await ctx.defer()
+		if ctx.author.bot:
+			return
+		if not await config.voted(ctx, bot=self.bot):
+			return await config.vtm(ctx)
+		if role == None:
+			role = ctx.guild.default_role
+
+		for hchannel in category.channels:
+		  overwrite = hchannel.overwrites_for(role)
+		  overwrite.update(send_messages=False)
+		  await hchannel.set_permissions(role, overwrite=overwrite)
+		  await sleep(2)
+		try:
+			await ctx.send(f'**<:vf:947194381172084767>Successfully Locked {category.name} From `{role.name}`**')
+		except:
+			return
+			
+
+
+	@cmd.hybrid_command(with_app_command = True, aliases=["ulc"])
+	@commands.has_permissions(manage_roles=True)
+	@commands.bot_has_permissions(manage_roles=True)
+	@commands.cooldown(2, 30, commands.BucketType.user)
+	@commands.guild_only()
+	async def unlock_category(self, ctx,category: discord.CategoryChannel, role:discord.Role=None):
+		await ctx.defer()
+		if ctx.author.bot:
+			return
+		if not await config.voted(ctx, bot=self.bot):
+			return await config.vtm(ctx)
+		if role == None:
+			role = ctx.guild.default_role
+
+		for hchannel in category.channels:
+		  overwrite = hchannel.overwrites_for(role)
+		  overwrite.update(send_messages=True, add_reactions=True)
+		  await hchannel.set_permissions(role, overwrite=overwrite)
+		  await sleep(2)
+		try:
+			await ctx.send(f'**<:vf:947194381172084767>Successfully Unlocked {category.name}**')
+		except:
+			return
+
+
+
+	@cmd.hybrid_command(with_app_command = True, aliases=["hc"])
+	@commands.has_permissions(manage_roles=True)
+	@commands.guild_only()
+	@commands.cooldown(2, 30, commands.BucketType.user)
+	@commands.bot_has_permissions(manage_roles=True)
+	async def hide_category(self, ctx,category: discord.CategoryChannel, role:discord.Role=None):
+		await ctx.defer()
+		if ctx.author.bot:
+			return
+		if not await config.voted(ctx, bot=self.bot):
+			return await config.vtm(ctx)
+		if role == None:
+			role = ctx.guild.default_role
+
+		
+		for hchannel in category.channels:
+		  overwrite = hchannel.overwrites_for(role)
+		  overwrite.update(view_channel=False)
+		  await hchannel.set_permissions(role, overwrite=overwrite)
+		  await sleep(2)
+		em = discord.Embed(description=f'**<:vf:947194381172084767> {category.name} is Hidden from `{role.name}`**', color=0x00ff00)
+		try:
+			await ctx.send(embed=em)
+		except:
+			return
+			#await sleep(1)
+
+
+
+	@cmd.hybrid_command(with_app_command = True, aliases=["uhc"])
+	@commands.has_permissions(manage_roles=True)
+	@commands.guild_only()
+	@commands.cooldown(1, 30, commands.BucketType.user)
+	@commands.bot_has_permissions(manage_roles=True)
+	async def unhide_category(self, ctx, category: discord.CategoryChannel, role :discord.Role = None):
+		await ctx.defer()
+		if ctx.author.bot:
+			return
+		if not await config.voted(ctx, bot=self.bot):
+			return await config.vtm(ctx)
+		if role == None:
+			role = ctx.guild.default_role
+
+		for uhchannel in category.channels:
+		  overwrite = uhchannel.overwrites_for(role)
+		  overwrite.update(view_channel=True)
+		  await uhchannel.set_permissions(role, overwrite=overwrite)
+		  await sleep(2)
+		em = discord.Embed(description=f'**<:vf:947194381172084767> {category.name} is Visible to `{role.name}`**', color=0x00ff00)
+		try:
+			await ctx.send(embed=em, delete_after=5)
+		except:
+			return
+
+
+
+
+
+	#clear command
+	@cmd.hybrid_command(aliases=['purge'], with_app_command = True)
+	@commands.has_permissions(manage_messages=True)
+	@commands.guild_only()
+	@commands.bot_has_permissions(manage_messages=True, send_messages=True)
+	#@commands.cooldown(2, 40, commands.BucketType.user)
+	async def clear(self, ctx, amount:int=None):
+		await ctx.defer()
+
+		if ctx.author.bot:
+			return
+		if not await config.voted(ctx, bot=self.bot):
+			return await config.vtm(ctx)
+		if amount == None:
+			amount = 10
+
+		await ctx.channel.purge(limit=amount)
+		return await ctx.send(f'**<:vf:947194381172084767> Successfully cleared {amount} messages**',delete_after=5)
+		
+
+
+
+	#Mute Command
+	@cmd.hybrid_command(with_app_command = True)
+	@commands.has_permissions(moderate_members=True)
+	@commands.bot_has_permissions(moderate_members=True)
+	@commands.guild_only()
+	@commands.cooldown(2, 30, commands.BucketType.user)
+	async def unmute(self, ctx, member: discord.Member, *, reason=None):
+		await ctx.defer()
+		if ctx.author.bot:
+			return
+		if not await config.voted(ctx, bot=self.bot):
+			return await config.vtm(ctx)
+		bt = ctx.guild.get_member(self.bot.user.id)
+		if reason == None:
+			reason = 'No reason provided'
+		if not ctx.author.top_role.position > member.top_role.position:
+			return await ctx.reply("You Can Not Manage Him")
+
+		if not bt.top_role.position > member.top_role.position:
+		    return await ctx.reply("I can't manage him")
+
+		else:
+		    time = humanfriendly.parse_timespan("0")
+		    await member.edit(timed_out_until=discord.utils.utcnow() + datetime.timedelta(seconds=time), reason=reason)
+		    try:
+		    	await ctx.send(f"{member} has been unmuted")
+		    except:
+		    	return	
+
+
+	@cmd.hybrid_command(with_app_command = True)
+	@commands.guild_only()
+	@commands.has_permissions(moderate_members=True)
+	@commands.cooldown(2, 30, commands.BucketType.user)
+	@commands.bot_has_permissions(moderate_members=True, send_messages=True)
+	async def mute(self, ctx, member: discord.Member, time=None, *, reason=None):
+		await ctx.defer()
+		if ctx.author.bot:
+			return
+		if not await config.voted(ctx, bot=self.bot):
+			return await config.vtm(ctx)
+		bt = ctx.guild.get_member(self.bot.user.id)
+		if time == None:
+			time = "5m"
+
+		if reason == None:
+			reason = 'No reason provided'
+
+		if not ctx.author.top_role.position > member.top_role.position:
+			try:
+				return await ctx.reply("You Can Not Manage Him")
+			except:
+				return
+
+		if not bt.top_role.position > member.top_role.position:
+		    try:
+		    	return await ctx.reply("I can't manage him")
+		    except:
+		    	return
+
+		else:
+		    timee = humanfriendly.parse_timespan(time)
+		    await member.edit(timed_out_until=discord.utils.utcnow() + datetime.timedelta(seconds=timee), reason=reason)
+		    try:
+		    	await ctx.send(f"{member} has been muted for {time}.\nReason: {reason}")
+		    except:
+		    	return
+
+
+
+
+	@cmd.hybrid_command(with_app_command = True)
+	@commands.has_permissions(kick_members=True)
+	@commands.bot_has_permissions(kick_members=True)
+	@commands.cooldown(2, 30, commands.BucketType.user)
+	@commands.guild_only()
+	async def kick(self, ctx, member: discord.Member, reason=None):
+		await ctx.defer()
+		if ctx.author.bot:
+			return
+		if not await config.voted(ctx, bot=self.bot):
+			return await config.vtm(ctx)
+		if reason == None:
+			reason = f"{member} kicked by {ctx.author}"
+
+		if ctx.author.top_role.position < member.top_role.position:
+			try:
+				return await ctx.send("You don't have enough permission")
+			except:
+				return
+
+		elif member == ctx.author:
+			try:
+				return await ctx.send("**You can't kick your self**")
+			except:
+				return
+
+		elif ctx.guild.me.top_role.position < member.top_role.position:
+			try:
+				return await ctx.send("**I can't kick him**")
+			except:
+				return
+
+		else:
+			return await ctx.guild.kick(member, reason=reason)
+			try:
+				return await ctx.send(f"{member} kicked")
+			except:
+				return
+
+
+	@cmd.hybrid_command(with_app_command = True)
+	@commands.bot_has_permissions(ban_members=True)
+	@commands.has_permissions(ban_members=True)
+	@commands.cooldown(2, 30, commands.BucketType.user)
+	@commands.guild_only()
+	async def ban(self, ctx, member: discord.Member, reason=None):
+		await ctx.defer()
+		if ctx.author.bot:
+			return
+		if not await config.voted(ctx, bot=self.bot):
+			return await config.vtm(ctx)
+		if reason == None:
+			reason = f"{member} banned by {ctx.author}"
+
+		if ctx.author.top_role.position < member.top_role.position:
+			try:
+				return await ctx.send(f"{member}'s role is higher than yours", delete_after=5)
+			except:
+				return
+
+		elif member == ctx.author:
+			try:
+				return await ctx.send("**You can't ban your self**", delete_after=5)
+			except:
+				return
+
+		elif ctx.guild.me.top_role.position < member.top_role.position:
+			try:
+				return await ctx.send("**I can't ban him**", delete_after=5)
+			except:
+				return
+
+		else:
+			await ctx.guild.ban(member, reason=reason)
+			try:
+				return await ctx.send(f"{member} banned", delete_after=5)
+			except:
+				return
+
+
+
+
+async def setup(bot):
+    await bot.add_cog(Moderation(bot))
---
+++ src/cogs/role.py
@@ -0,0 +1,403 @@
+import discord, os, asyncio
+from discord.ext import commands
+from modules import config
+
+
+cmd = commands
+blurple = 0x7289da
+greyple = 0x99aab5
+d_grey = 0x546e7a
+d_theme = 0x36393F
+l_grey = 0x979c9f
+d_red = 0x992d22
+red = 0xe74c3c
+d_orange = 0xa84300
+orange= 0xe67e22
+d_gold = 0xc27c0e
+gold = 0xf1c40f
+magenta = 0xe91e63
+purple = 0x9b59b6
+d_blue = 0x206694 
+blue = 0x3498db
+green = 0x2ecc71
+d_green = 0x1f8b4c
+cyan = 0x1abc9c
+d_dcyan = 0x11806a
+
+
+class Roles(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+
+	@cmd.command(aliases=["croles"])
+	@commands.has_permissions(manage_roles=True)
+	@commands.bot_has_permissions(manage_roles=True)
+	async def create_roles(self, ctx, *Names:str):
+		if ctx.author.bot:
+			return
+		if not await config.voted(ctx, bot=self.bot):
+			return await config.vtm(ctx)
+		for role in Names:
+			await ctx.guild.create_role(name=role, reason=f"Created by {ctx.author}")
+			await asyncio.sleep(2)
+		await ctx.send(embed=discord.Embed(description=f"{config.tick} | All roles created"), delete_after=5)
+
+
+
+
+	@cmd.command(aliases=["droles"])
+	@commands.has_permissions(manage_roles=True)
+	@commands.bot_has_permissions(manage_roles=True)
+	async def del_roles(self, ctx, *roles : discord.Role):
+		if ctx.author.bot:
+			return
+		if not await config.voted(ctx, bot=self.bot):
+			return await config.vtm(ctx)
+			
+		bt = ctx.guild.get_member(self.bot.user.id)
+		msg = await ctx.send(f"{config.loading} Processing...")
+		for role in roles:
+			if ctx.author.top_role.position < role.position:
+				return await ctx.send("Role Is Higher Than Your Top Role", delete_after=5)
+
+			elif bt.top_role.position < role.position:
+				return await ctx.send("Role Is Higher Than My Top Role", delete_after=5)
+
+			else:
+				await role.delete(reason=f"Role {role.name} has been deleted by {ctx.author}")
+				await asyncio.sleep(2)
+		await msg.edit(content=None, embed=discord.Embed(color=config.cyan, description=f"{config.tick} | Roles Successfully Deleted", delete_after=30))
+
+
+
+
+
+	@cmd.command(aliases=["role"])
+	@commands.has_permissions(manage_roles=True)
+	@commands.bot_has_permissions(manage_roles=True)
+	async def give_role(self, ctx, role: discord.Role, *users: discord.Member):
+		if ctx.author.bot:
+			return
+		if not await config.voted(ctx, bot=self.bot):
+			return await config.vtm(ctx)
+			
+		ms = await ctx.send("Processing...")
+		bt = ctx.guild.get_member(self.bot.user.id)
+		given = []
+		if bt.top_role.position <= role.position:
+			if ms:
+				await ms.edit(content="My Top Role position Is not higher enough")
+		if not ctx.author.top_role.position >= role.position:
+			if ms:
+				return await ms.edit(content="You can Not manage that role")
+
+		if users:
+			if len(users) >=1 and role.permission.administrator:
+				return await ms.edit(content="**I can't give admin role to more than 1 person. at a time**")
+			for user in users:
+				if user.top_role.position >= ctx.author.top_role.position:
+					try:
+						await ms.edit(content=f"{user}'s Role Is Higher Than __Your Top Role__! I can not manage him")
+					except:
+						return
+
+				if bt.top_role.position < user.top_role.position:
+					try:
+						await ms.edit(content=f"{user}'s Role Is Higher Than __My Top Role__! I can not manage him")
+					except:
+						return
+
+				else:
+					await user.add_roles(role)
+					given.append(user)
+					await asyncio.sleep(2)
+
+			if ms:
+				await ms.edit(content=f"{role.mention} given To {len(given)} Members")
+
+		if not users and ctx.message.reference:
+			message = await ctx.channel.fetch_message(ctx.message.reference.message_id)
+			for user in ctx.guild.members:
+				if user.mention in message.content.split():
+					if user.top_role.position >= ctx.author.top_role.position:
+						try:
+							await ms.edit(content=f"{user}'s Role Is Equal Or Higher Than __Your Top Role__! I can not manage him")
+						except:
+							return
+
+
+					if bt.top_role.position <= user.top_role.position:
+						try:
+							await ms.edit(content=f"{user}'s Role Is Equal Or Higher Than __My Top Role__! I can not manage him")
+						except:
+							return
+
+
+
+					else:
+						await user.add_roles(role)
+						given.append(user)
+						await asyncio.sleep(2)
+
+			if ms:
+				await ms.edit(content=f"Role Added To - {len(given)} Members")
+
+
+
+
+
+	@cmd.command(aliases=["ra_role"])
+	@commands.has_permissions(administrator=True)
+	@commands.guild_only()
+	@commands.bot_has_permissions(manage_roles=True)
+	async def remove_role_members(self, ctx, role: discord.Role, reason=None):
+		if ctx.author.bot:
+			return
+		prs = await ctx.send("<a:loading:969894982024568856> | Processing...")
+		if reason == None:
+			reason = f"{role} removed by {ctx.author}"
+			
+		for member in role.members:
+			await member.remove_roles(role, reason=reason)
+			await asyncio.sleep(2)
+			
+		return await prs.edit(content=f"**:white_check_mark: | {role} Removed from everyone**", delete_after=30)
+
+
+
+	@cmd.hybrid_command(with_app_command = True)
+	@commands.guild_only()
+	@commands.bot_has_permissions(manage_roles=True)
+	@commands.cooldown(1, 15, commands.BucketType.user)
+	async def inrole(self, ctx, role: discord.Role):
+		await ctx.defer()
+		if ctx.author.bot:
+			return
+		if not await config.voted(ctx, bot=self.bot):
+			return await config.vtm(ctx)
+			
+		msg = ""
+		if len(role.members) < 15:
+			for i in role.members:
+				msg = msg + f"\n{i} : {i.id}"
+			try:
+				await ctx.send(msg)
+			except:
+				return
+
+		if len(role.members) > 15 and len(role.members) <= 1000000:
+			for i in role.members:
+				msg = msg + f"\n{i} : {i.id}"
+			file = open("members.txt", "w", encoding="utf-8")
+			file.write(msg)
+			file.close()
+			await ctx.send(f"total members : `{len(role.members)}`",file=discord.File("members.txt"))
+			os.remove("members.txt")
+		if len(role.members) > 2000000:
+			try:
+				return await ctx.send("Too Many Members To Show!")
+			except:
+				return
+
+
+	@cmd.hybrid_command(with_app_command = True)
+	@commands.has_permissions(manage_roles=True)
+	@commands.bot_has_permissions(manage_roles=True)
+	@commands.guild_only()
+	async def port(self, ctx, role1: discord.Role, role2: discord.Role, reason=None):
+		await ctx.defer()
+		if ctx.author.bot:
+			return
+		if not await config.voted(ctx, bot=self.bot):
+			return await config.vtm(ctx)
+			
+		bt = ctx.guild.get_member(self.bot.user.id)
+
+		if role2.position > bt.top_role.position:
+			return await ctx.send("I Can't Manage This Role, It is Higher Than My Top Role")
+
+		if role2.position > ctx.author.top_role.position:
+			return await ctx.send("You Can't Manage This Role")
+
+		await ctx.reply("If You're Running this command by mistake! You Can Run `&help ra_role`")
+
+		if reason == None:
+			reason = f"{role2.name} added by {ctx.author}"
+			
+		msg = await ctx.send(f"**{config.loading} Processing...**")
+
+		for m in role1.members:
+			if m.top_role.position < bt.top_role.position:
+				await m.add_roles(role2, reason=reason)
+				await asyncio.sleep(1)
+
+		await msg.edit(content=f"{config.vf} **Role Added Successfully.**", delete_after=30)
+
+
+
+	@cmd.command()
+	@commands.has_permissions(manage_roles=True)
+	@commands.bot_has_permissions(manage_roles=True)
+	@commands.guild_only()
+	async def remove_role(self, ctx, role:discord.Role, *users: discord.Member):
+		if ctx.author.bot:
+			return
+		if not await config.voted(ctx, bot=self.bot):
+			return await config.vtm(ctx)
+		bt = ctx.guild.get_member(self.bot.user.id)
+		for user in users:
+			if ctx.author.top_role.position < user.top_role.position:
+				return await ctx.send(f"**You don't have enough permission to manage {user}'s role'**", delete_after=15)
+
+
+			if bt.top_role.position < user.top_role.position:
+				return await ctx.send(f"**I can't manage {user}'r role'**", delete_after=15)
+
+			if bt.top_role.position < role.position:
+				return await ctx.send("**I don't have enough permission to manage this role**", delete_after=15)
+
+			else:
+				await user.remove_roles(role, reason=f"Role removed by {ctx.author}")
+		return await ctx.send(f"**{role.name} removed from these members**")
+
+
+
+	@cmd.command()
+	@commands.guild_only()
+	@commands.has_permissions(manage_roles=True)
+	@commands.bot_has_permissions(manage_roles=True)
+	async def add_roles(self, ctx, user:discord.Member, * roles:discord.Role):
+		if ctx.author.bot:
+			return
+		if not await config.voted(ctx, bot=self.bot):
+			return await config.vtm(ctx)
+		bt = ctx.guild.get_member(self.bot.user.id)
+		for role in roles:
+			if bt.top_role.position > role.position:
+				return await ctx.send("**My top role is not higher enough**", delete_after=20)
+
+			if ctx.author.top_role.position < role.position:
+				return await ctx.send("you don't have enough permission", delete_after=5)
+
+			if user.top_role.position > ctx.author.top_role.position:
+				return await  ctx.send("Your can not manage him")
+
+			else:
+				await user.add_roles(role)
+
+		return await ctx.message.add_reaction("✅")
+
+
+
+
+
+
+	@cmd.hybrid_command(with_app_command = True)
+	@commands.guild_only()
+	@commands.has_permissions(manage_roles=True)
+	@commands.bot_has_permissions(manage_roles=True)
+	async def role_all_human(self, ctx, role: discord.Role):
+		if ctx.author.bot:
+			return
+		await ctx.defer()
+		if ctx.author.bot:
+			return
+		prs = await ctx.send("Processing...")
+		if role.permissions.administrator:
+			return await prs.edit(content="**Sorry but i can not do this with a role with admin perms.**")
+		if ctx.author.top_role.position <= role.position:
+			return await prs.edit(content=f"**{config.cross}You Can't Manage This Role | The role should be higher than your top role.**")
+	
+		if discord.utils.get(ctx.guild.members, id=self.bot.user.id).top_role.position < role.position:
+			return await prs.edit(content="I can't manage This role")
+		if len(ctx.guild.members) != ctx.guild.member_count:
+			return await prs.edit(content="**I'm unable to see anyone! i don't know why. please contact support team!**")
+		for member in ctx.guild.members:
+			if not member.bot:
+				await member.add_roles(role, reason=f"role all command used by {ctx.author}")
+				await asyncio.sleep(3)
+		await prs.edit(content=None, embed=discord.Embed(color=0x00ff00, description=f"**{config.tick} | {role.mention} Given To All These Humans**"))
+
+
+
+
+
+	@cmd.hybrid_command(with_app_command = True)
+	@commands.guild_only()
+	@commands.has_permissions(manage_roles=True)
+	@commands.bot_has_permissions(manage_roles=True)
+	async def role_all_bot(self, ctx, role: discord.Role):
+		await ctx.defer()
+		if ctx.author.bot:
+			return
+		prs = await ctx.send("Processing...")
+		if role.permissions.administrator:
+			return await prs.edit(content="**Sorry but i can not do this with a role with admin perms.**")
+		if ctx.author.top_role.position <= role.position:
+			return await prs.edit(content="You Can't Manage This Role")
+	
+		if discord.utils.get(ctx.guild.members, id=self.bot.user.id).top_role.position < role.position:
+			return await prs.edit(content="I can't manage This role")
+
+		if len(ctx.guild.members) != ctx.guild.member_count:
+			return await prs.edit(content="**I'm unable to see anyone! i don't know why. please contact support team!**")
+
+		for member in ctx.guild.members:
+			if member.bot:
+				await member.add_roles(role, reason=f"role all command used by {ctx.author}")
+				await asyncio.sleep(3)
+		await prs.edit(content=None, embed=discord.Embed(color=0x000fff, description=f"**{config.tick} | {role.mention} Given To All Bots**"))
+
+
+
+
+
+	@cmd.hybrid_command(with_app_command = True)
+	@commands.guild_only()
+	@commands.has_permissions(manage_roles=True)
+	@commands.bot_has_permissions(manage_roles=True)
+	async def hide_roles(self, ctx):
+		await ctx.defer()
+		if ctx.author.bot:
+			return
+		msg = await ctx.send(f'{config.loading}** Processing..**')
+		roles = ctx.guild.roles
+		for role in roles:
+		    if role.position < ctx.author.top_role.position:
+		            try:
+		                await role.edit(hoist=False)
+		            except:
+		                pass
+		await msg.edit(content=f"{config.vf} Done", delete_after=10)
+
+
+
+
+	@cmd.command()
+	@commands.guild_only()
+	@commands.has_permissions(manage_roles=True)
+	@commands.bot_has_permissions(manage_roles=True)
+	async def unhide_roles(self, ctx, *roles : discord.Role):
+		if ctx.author.bot:
+			return
+		msg = await ctx.send(f'{config.loading}** Processing..**')
+		for role in roles:
+		    if role.position < ctx.author.top_role.position:
+		        try:
+		            await role.edit(hoist=True)
+		        except:
+		            pass
+		await msg.edit(content=f"{config.vf} Done", delete_after=10)
+
+
+
+
+
+
+
+
+
+
+
+async def setup(bot):
+    await bot.add_cog(Roles(bot))
