ae4621b89201c806a47777f02dd9325ca7a740c5
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2024-01-07 20:03:31 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -365,5 +365,5 @@ def homee():return "Online..!"
 def run():ap.run(host='0.0.0.0', port=8080)
 def keep_alive():t = Thread(target=run); t.start()
 
-if __name__ == '__main__':
-	ap.run(host="0.0.0.0", port=8080)
+# if __name__ == '__main__':
+# 	ap.run(host="0.0.0.0", port=8080)
