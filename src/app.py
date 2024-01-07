ef734d4055be9f0a8f9d93e36ef2b39066b48381
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2024-01-07 14:44:03 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -363,7 +363,14 @@ def send():
 	  "username": data["name"]
 	 })
 	return "Sent"
+	
+def run():
+	app.run(host='0.0.0.0', port=8080)
 
+
+def keep_alive():
+	t = Thread(target=run)
+	t.start()
 
 if __name__ == '__main__':
 	app.run(host="0.0.0.0", port=8080)
