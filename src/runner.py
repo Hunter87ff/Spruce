207e792e52d5388377a03ca4c62cab1112d1bba9
hunter87ff <huntergaming702@gmail.com>
2024-07-05 23:55:22 +0530

    update

--- src/runner.py
+++ src/runner.py
@@ -21,9 +21,10 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-import os
+import os, time
 from threading import Thread
 def lavalink():os.system("cd lava && java -jar Lavalink.jar > /dev/null 2>&1 &")
 Thread(target=lavalink).start()
+time.sleep(5)
 try:os.system("python src/main.py")
 except:os.system("python3 src/main.py")
