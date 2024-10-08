584de702e724b0f7af89896aa26ea50abdb6a70a
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2024-10-08 20:34:34 +0530

    Update chat.py

--- src/modules/chat.py
+++ src/modules/chat.py
@@ -1,3 +1,12 @@
+"""
+                    GNU GENERAL PUBLIC LICENSE
+                       Version 3, 29 June 2007
+
+ Copyright (C) 2022 souravyt87@gmail.com
+ Everyone is permitted to copy and distribute verbatim copies
+ of this license document, but changing it is not allowed.
+ """
+
 from modules import config
 from discord import Message, File
 from discord.ext import commands
