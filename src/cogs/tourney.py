6ad75b859fbf9477a2d09e5c1b058f0d8b678d9a
hunter87ff <huntergaming702@gmail.com>
2025-05-29 01:55:35 +0530

    Refactor license headers in multiple files for consistency and clarity.

--- src/cogs/tourney.py
+++ src/cogs/tourney.py
@@ -1,11 +1,9 @@
 """
-                    GNU GENERAL PUBLIC LICENSE
-                       Version 3, 29 June 2007
-
- Copyright (C) 2022 hunter87.dev@gmail.com
- Everyone is permitted to copy and distribute verbatim copies
- of this license document, but changing it is not allowed.
- """
+This project is licensed under the GNU GPL v3.0.
+Copyright (C) 2022 hunter87.dev@gmail.com
+Everyone is permitted to copy and distribute verbatim copies
+of this license document, but changing it is not allowed.
+"""
 
 
 import discord
