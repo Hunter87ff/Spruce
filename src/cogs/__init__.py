d9a27a4062d07bb4115ffec068b13399b77fc768
hunter87ff <huntergaming702@gmail.com>
2025-06-14 22:42:49 +0530

    refactor: Update import statements for consistency and clarity; hide developer commands in DevCog; enhance role management commands with better error handling and descriptions

--- src/cogs/__init__.py
+++ src/cogs/__init__.py
@@ -10,14 +10,14 @@ and attach these cogs to the bot instance.
 """
 from modules.config import IS_DEV_ENV
 from typing import TYPE_CHECKING
-from cogs.dev import DevCog
-from cogs.esports import EsportsCog
-from cogs.helpcog import HelperCog
-from cogs.moderation import ModerationCog
-from cogs.role import RoleCog
-from cogs.tasks import TaskCog
-from cogs.utility import UtilityCog
-from cogs.esports import ScrimCog
+from .dev import DevCog
+from .esports import EsportsCog, ScrimCog
+from .helpcog import HelperCog
+from .moderation import ModerationCog
+from .role import RoleCog
+from .tasks import TaskCog
+from .utility import UtilityCog
+from .esports import ScrimCog
 from discord.ext.commands import Cog
 
 if TYPE_CHECKING:
