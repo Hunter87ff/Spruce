b5d3e0a4f615f61ebdfd801fd0e0df7d7031772f
hunter87ff <huntergaming702@gmail.com>
2025-06-19 12:27:37 +0530

    fix: Prevent NoneType error by checking if interaction response is already sent before sending tournament availability message

--- src/cogs/esports/tourney.py
+++ src/cogs/esports/tourney.py
@@ -1488,7 +1488,10 @@ class EsportsCog(commands.Cog):
         
         db:dict = self.bot.db.dbc.find_one({"mch":interaction.channel.id})
         if not db:
-            return await interaction.response.send_message("Tournament is No Longer Available!!", ephemeral=True)
+            return await interaction.response.send_message(
+                "Tournament is No Longer Available!!", 
+                ephemeral=True
+            ) if not interaction.response.is_done() else None # fixes  NoneType error when the interaction response is already sent
         
         view = View()
         crole:discord.Role = interaction.guild.get_role(db["crole"])
