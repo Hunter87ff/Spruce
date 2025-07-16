2d91b0f33d93ee61ebca3f968e121e2fa2de9064
hunter87ff <huntergaming702@gmail.com>
2025-07-16 19:30:29 +0530

    Refactor TourneyCog and TourneyModel: Improve channel creation and update ephemeral message response; enhance ConfirmedTeamModel with validation and new methods for tournament management

--- src/models/tourney.py
+++ src/models/tourney.py
@@ -1,13 +1,13 @@
-from datetime import datetime
+import time
 from typing import TYPE_CHECKING
 from typing import Unpack, TypedDict
 
 if TYPE_CHECKING:
-    from pymongo.collection import Collection
+    from discord import Message, Member
     from pymongo.asynchronous.collection import AsyncCollection
+    from core.bot import Spruce
 
 _tourney_cache: dict[int, 'TourneyModel'] = {}
-
 
 class TournamentPayload(TypedDict):
     guild_id: int
@@ -21,9 +21,47 @@ class TournamentPayload(TypedDict):
     published: str | None
     slot_per_group: int
     current_group: int | None
-    created_at: datetime
-    col : "Collection"
+    created_at: int
 
+
+class ConfirmedTeamModel:
+    def __init__(self, message: 'Message'):
+        self.message: 'Message' = message
+        if not isinstance(message, Message):
+            raise TypeError("message must be an instance of discord.Message")
+        
+        if message.author.id != message.guild.me.id:
+            raise ValueError("message must be sent by the bot itself")
+        
+        if len(message.mentions) != 1 or not message.embeds:
+            raise ValueError("message must mention exactly one user and contain an embed")
+        
+        self.captain = message.mentions[1]
+        self.name = message.content.replace(self.captain.mention, "").strip() if message.content else f"{self.captain.name}'s Team"
+        self.players = message.embeds[0].description.split("\n")[1].replace("Players: ", "").split(", ") if message.embeds[0].description else []
+
+
+    
+    async def edit(self, name: str = None, captain: "Member" = None):
+        """Edit the team name and/or players in the confirmation message."""
+
+        content = f"{name or self.name} {captain.mention or self.captain.mention}"
+        embed = self.message.embeds[0]
+        embed.description = embed.description.replace(self.name, name) if name else embed.description
+ 
+        self.name = name if name else self.name
+        self.captain = captain if captain else self.captain
+        
+        await self.message.edit(content=content, embed=embed)
+
+    async def delete(self):
+        """Delete the confirmation message."""
+        if self.message:
+            await self.message.delete()
+            self.message = None
+            del self
+        else:
+            raise ValueError("Message has already been deleted or does not exist.")
 
 
 class TourneyModel:
@@ -31,32 +69,32 @@ class TourneyModel:
     Tourney class represents a tournament with various properties.
     """
     col: "AsyncCollection" = None  # MongoDB collection name for tournaments
+    bot : "Spruce" = None  # Reference to the bot instance
 
     def __init__(self, **kwargs: Unpack[TournamentPayload]) -> None:
         self.guild_id: int = kwargs.get("guild_id")
         self.name: str = kwargs.get("name", "Tournament")
-        self.reg_channel: int = kwargs.get("reg_channel")
-        self.slot_channel: int = kwargs.get("slot_channel")
-        self.group_channel: int = kwargs.get("group_channel")
-        self.slot_manager: int = kwargs.get("slot_manager", None) #slot manager channel id
+        self.reg_channel: int = kwargs.get("rch")
+        self.slot_channel: int = kwargs.get("cch")
+        self.group_channel: int = kwargs.get("gch")
+        self.slot_manager: int = kwargs.get("mch", None) #slot manager channel id
         self.mentions: int = kwargs.get("mentions", 4) # minimum mentions required to register
-        self.confirm_role: int = kwargs.get("confirm_role") # confirmation role id
-        self.total_slots: int = kwargs.get("total_slot") # total slots available in the tournament
-        self.team_count: int = kwargs.get("team_count", 0) # number of teams registered in the tournament
-        self.auto_group: bool = kwargs.get("auto_group", False) # whether the tournament has auto grouping enabled (deprecated)
+        self.confirm_role: int = kwargs.get("crole") # confirmation role id
+        self.total_slots: int = kwargs.get("slots") # total slots available in the tournament
+        self.team_count: int = kwargs.get("reged", 0) # number of teams registered in the tournament
         self.published: str = kwargs.get("published", None)
-        self.slot_per_group: int = kwargs.get("slot_per_group", 12) #number of slots per group (default: 12)
+        self.slot_per_group: int = kwargs.get("spg", 12) #number of slots per group (default: 12)
         self.current_group: int = kwargs.get("current_group", 0) # current group number (default: 0)
-        self.created_at: datetime = kwargs.get("created_at", datetime.now())
+        self.created_at: int = kwargs.get("created_at", int(time.time()))
 
         if kwargs.get("col"):
             self.col = kwargs.get("col", None)
 
     def __repr__(self) -> str:
-        return f"<Tournament guild_id={self.guild_id} rch={self.reg_channel} cch={self.slot_channel} crole={self.confirm_role} gch={self.group_channel} tslot={self.total_slots} reged={self.team_count} auto_grp={self.auto_group} pub={self.published} spg={self.slot_per_group} cgp={self.current_group} created_at={self.created_at}>"
+        return f"<Tournament guild_id={self.guild_id} rch={self.reg_channel} cch={self.slot_channel} crole={self.confirm_role} gch={self.group_channel} tslot={self.total_slots} reged={self.team_count} pub={self.published} spg={self.slot_per_group} cgp={self.current_group} created_at={self.created_at}>"
     
     def __str__(self) -> str:
-        return f"Tournament(guild_id={self.guild_id}, reg_channel={self.reg_channel}, slot_channel={self.slot_channel}, confirm_role={self.confirm_role}, group_channel={self.group_channel}, total_slot={self.total_slots}, team_count={self.team_count}, auto_group={self.auto_group}, published={self.published}, slot_per_group={self.slot_per_group}, current_group={self.current_group}, created_at={self.created_at})"
+        return f"Tournament(guild_id={self.guild_id}, reg_channel={self.reg_channel}, slot_channel={self.slot_channel}, confirm_role={self.confirm_role}, group_channel={self.group_channel}, total_slot={self.total_slots}, team_count={self.team_count}, published={self.published}, slot_per_group={self.slot_per_group}, current_group={self.current_group}, created_at={self.created_at})"
 
 
     def __eq__(self, value):
@@ -64,8 +102,6 @@ class TourneyModel:
             return self.reg_channel == value
         elif isinstance(value, TourneyModel):
             return self.reg_channel == value.reg_channel and self.guild_id == value.guild_id
-        elif isinstance(value, datetime):
-            return self.created_at == value
         return False
 
 
@@ -83,11 +119,10 @@ class TourneyModel:
             isinstance(self.group_channel, int),
             isinstance(self.total_slots, int) and self.total_slots > 0,
             isinstance(self.team_count, int) and self.team_count >= 0,
-            isinstance(self.auto_group, bool),
             isinstance(self.published, (str, type(None))),
-            isinstance(self.slot_per_group, int) and self.slot_per_group > 0,
+            isinstance(self.slot_per_group, int) and self.slot_per_group > 0 and self.slot_per_group <= 100,
             isinstance(self.current_group, (int, type(None))),
-            isinstance(self.created_at, datetime)
+            isinstance(self.created_at, int)
         ])
 
 
@@ -105,13 +140,14 @@ class TourneyModel:
             "group_channel": self.group_channel,
             "total_slot": self.total_slots,
             "team_count": self.team_count,
-            "auto_group": self.auto_group,
             "published": self.published,
             "slot_per_group": self.slot_per_group,
             "current_group": self.current_group,
             "slot_manager": self.slot_manager,
             "created_at": self.created_at
         }
+    
+
     
     async def save(self):
         if not self.validate():
@@ -160,3 +196,47 @@ class TourneyModel:
         tournament = cls(**data)
         _tourney_cache[reg_channel] = tournament
         return tournament
+    
+
+    @classmethod
+    async def create(cls, **kwargs: Unpack[TournamentPayload]) -> 'TourneyModel':
+        """Creates a new Tournament instance and saves it to the database.
+
+        Args:
+            **kwargs: Keyword arguments to initialize the tournament.
+
+        Returns:
+            TourneyModel: The created TournamentModel instance.
+        """
+        if not kwargs.get("guild_id") or not kwargs.get("reg_channel"):
+            raise ValueError("guild_id and reg_channel are required to create a tournament.")
+
+        existing_tournament = await cls.find_one({"reg_channel": kwargs["reg_channel"], "guild_id": kwargs["guild_id"]})
+        if existing_tournament:
+            return existing_tournament
+
+        tournament = cls(**kwargs)
+        await tournament.save()
+        _tourney_cache[tournament.reg_channel] = tournament
+        return tournament
+    
+
+    @classmethod
+    async def delete(cls, reg_channel: int) -> bool:
+        """Deletes a Tournament instance from the database.
+
+        Args:
+            reg_channel (int): The registration channel ID of the tournament to delete.
+
+        Returns:
+            bool: True if the tournament was deleted, False otherwise.
+        """
+        if reg_channel not in _tourney_cache:
+            return False
+        
+        _tourney_cache.pop(reg_channel, None)
+        result = await cls.col.delete_one({"reg_channel": reg_channel})
+        if result.deleted_count > 0:
+            _tourney_cache.pop(reg_channel, None)
+            return True
+        return False
