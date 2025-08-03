bda891cc95a656cdd351f45edcc3f22cedd3b8f7
hunter87ff <huntergaming702@gmail.com>
2025-08-03 21:08:24 +0530

    Refactor imports in __init__.py: reorder scrim handlers and update __all__ exports

--- src/events/esports/__init__.py
+++ src/events/esports/__init__.py
@@ -1,9 +1,11 @@
 from .tourney import handle_tourney_registration
-from .scrim import handle_scrim_registration
+from .scrim import handle_scrim_registration, handle_scrim_end, handle_scrim_start
+from .scrim.interaction import handle_scrim_slot_manager_interaction
 
 __all__ = (
-    "handle_tourney_registration",
-    "handle_scrim_registration",
     "handle_scrim_start",
     "handle_scrim_end",
+    "handle_tourney_registration",
+    "handle_scrim_registration",
+    "handle_scrim_slot_manager_interaction",
 )
